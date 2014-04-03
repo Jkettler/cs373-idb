21e7fd35a0054cf98c1593f2a886c1a8c7f12b45
Eliya Shabanov <eliya_shab@yahoo.com>
2014-04-03 14:58:53 -0500

    Fixed the issue with senators not showing the bils they've authored

--- idb/views.py
+++ idb/views.py
@@ -49,7 +49,7 @@ class SenatorView(TemplateView):
         context = super(SenatorView, self).get_context_data(**kwargs)
         senator = Senator.objects.get(id=str(self.args[0]))
         context['senator'] = senator
-        context['bills'] = senator.voted_bill_set.all()
+        context['bills'] = senator.bill_set.all()
         context['chair'] = senator.committee_chair_set.all()
         context['vice_chair'] = senator.committee_vice_chair_set.all()
         context['committees'] = senator.senator_set.all() 
