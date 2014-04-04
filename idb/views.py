0b958c8b714114a92522b1c75158bacfa5ecfd7d
Jkettler <jimmy@schooladmin.com>
2014-04-04 11:03:32 -0500

    fixed senator view

--- idb/views.py
+++ idb/views.py
@@ -94,9 +94,9 @@ class SenatorView(TemplateView):
         context['bills'] = senator.bill_set.all()
         context['chair'] = senator.committee_chair_set.all()
         context['vice_chair'] = senator.committee_vice_chair_set.all()
-        context['committees'] = senator.senator_set.all()
-        context['img_counter'] = enumerate(senator.picture_set.all())
-        context['img_links'] = enumerate(senator.picture_set.all())
+        context['committees'] = senator.committee_set.all()
+        context['img_counter'] = enumerate(senator.pictures_set.all())
+        context['img_links'] = enumerate(senator.pictures_set.all())
         # context['committees'] = senator.senator_set.all()
         return context
 
